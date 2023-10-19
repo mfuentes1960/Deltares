5c74388ec8be2d70713e1be10afcdc7459af27a1
Mario Fuentes Aguilar <mariofuentesaguilar@hotmail.com>
2023-10-19 14:40:54 -0600

    Cambio del día de hoy
    Prueba

--- Postgresql/P001_Deltares/PA004_Result/views.py
+++ Postgresql/P001_Deltares/PA004_Result/views.py
@@ -8,7 +8,7 @@ def result(request):
     gppd=GPPD.objects.all()
 
     return render(request,"PA004_Result/result.html",{"gppd": gppd})
-
+# segunda prueba 
 def consultResult(request, code):
     consult= resultfile.objects.filter(id_GPPD=code)
     return render(request,"PA004_Result/resultConsult.html",{"consult": consult})
